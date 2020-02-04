3c2afe185737652132f91113355098d3d262e211
韩灵稚 <hanlingzhi@hanlingzhiMacBook-Pro.local>
2020-02-04 23:09:51 +0800

    增加已有产品线的过滤

--- test_spider/spiders/aliyun_product_spider.py
+++ test_spider/spiders/aliyun_product_spider.py
@@ -3,7 +3,7 @@ import json
 from scrapy.http import Request
 
 from test_spider.util.constant import CONST
-# from mobile_price_spider.db import spider_db
+from test_spider.orm import spider_db
 from test_spider.items import ProductItem
 from test_spider.util.print_format_util import PrintFormatUtil
 
@@ -22,7 +22,10 @@ class ALIProductSpider(scrapy.Spider):
 
     allowed_domains = [CONST.ALI_YUN_DOMAIN]  # 接受处理的域名
 
+    product_list = spider_db.get_product_all()
+
     def start_requests(self):
+        PrintFormatUtil.print_line("阿里云已有的服务一共{}个".format(len(self.product_list)))
         yield Request(CONST.ALI_YUN_PRODUCT_URL, callback=self.parse, headers=CONST.ALI_YUN_HEADER)
 
     def parse(self, response):
@@ -30,7 +33,7 @@ class ALIProductSpider(scrapy.Spider):
         assert 'product' in data and len(data['product']) > 0, "URL{} 数据不符合要求 ".format(response.url)
         for category_1 in data['product']:
             if 'name' in data['product'][category_1]:
-                PrintFormatUtil.print_line("获取一级类目{}".format(category_1))
+                PrintFormatUtil.print_line("处理一级类目{}".format(category_1))
                 for service in data['product'][category_1]['name']:
                     item = ProductItem()
                     item['category1'] = service['category1']
@@ -41,5 +44,6 @@ class ALIProductSpider(scrapy.Spider):
                     item['title'] = service['title']
                     item['description'] = service['description']
                     item['link'] = service['textLink']
-                    PrintFormatUtil.print_line("获取子服务{}".format(item))
-                    yield item
+                    if "%s:%s" % (service['category1'], service['title']) not in self.product_list:
+                        PrintFormatUtil.print_line("新增获取子服务{}".format(item))
+                        yield item
