312757df7f604485fc716f31c4c756262ac4024c
韩灵稚 <hanlingzhi@hanlingzhiMacBook-Pro.local>
2020-02-04 23:14:15 +0800

    修改spriderloader

--- test_spider/commands/crawlall.py
+++ test_spider/commands/crawlall.py
@@ -1,6 +1,7 @@
 from scrapy.commands import ScrapyCommand
 
 class Command(ScrapyCommand):
+
     requires_project = True
 
     def syntax(self):
@@ -10,8 +11,8 @@ class Command(ScrapyCommand):
         return 'Runs all of ahs the spiders'
 
     def run(self, args, opts):
-        spider_list = self.crawler_process.spiders.list()
+        spider_list = self.crawler_process.spider_loader.list()
         for name in spider_list:
-            if str(name).startswith("aliyun"):
+            if str(name).startswith("ali"):
                 self.crawler_process.crawl(name, **opts.__dict__)
         self.crawler_process.start()
