79d29c4acc93513b283a2189f17034e3d39073d2
韩灵稚 <hanlingzhi@hanlingzhiMacBook-Pro.local>
2020-02-07 18:13:06 +0800

    增加driver的常量参数

--- test_spider/util/constant.py
+++ test_spider/util/constant.py
@@ -28,13 +28,38 @@ CONST.ALI_YUN_PRODUCT_URL = 'https://query.aliyun.com/rest/topbar.view.product.a
 
 CONST.ALI_YUN_PRODUCT_SPIDER_NAME = 'ali_product_spider'
 
+#CONST.ALI_YUN_M_URL = 'https://m.aliyun.com'
+
+CONST.ALI_YUN_M_URL = 'https://www.aliyun.com/product/petadata'
+
+CONST.ALI_YUN_M_SPIDER_NAME = 'ali_m_spider'
+
 CONST.ALI_YUN_HEADER = {
     'Content-type': 'application/json; charset=utf-8',
 }
 
+CONST.PIC_PATH = 'test_spider/pic'
+
+CONST.CHROME_DRIVER_BIN_PATH = 'test_spider/bin/chromedriver'
+
+CONST.CHROME_DRIVER_OPTIONS = [
+    "start-maximized",
+    "enable-automation",
+    "--headless",
+    "--no-sandbox",
+    "--disable-infobars",
+    '--disable-dev-shm-usage',
+    "--disable-browser-side-navigation",
+    "--disable-gpu",
+    "--disable-extensions",
+    "--dns-prefetch-disable",
+]
+
+CONST.CHROME_DRIVER_PAGE_LOAD_TIMEOUT = 200
+
 CONST.SPIDER_DB_INFO = {
     'database': 'qa_plat',
-    'host': '**********',
+    'host': 'dev-mysql.danke.life',
     'user': 'dev_qa_plat_writer',
     'password': 'VUDQWApJs3ZGT7Js',
     'port':3306,
