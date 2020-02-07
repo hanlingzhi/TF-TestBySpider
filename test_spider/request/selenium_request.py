c5e74a822527f02fed3496b67c5cb5e1bb62408e
韩灵稚 <hanlingzhi@hanlingzhiMacBook-Pro.local>
2020-02-07 18:16:30 +0800

    override selenium request class

---
+++ test_spider/request/selenium_request.py
@@ -0,0 +1,33 @@
+"""This module contains the ``SeleniumRequest`` class"""
+
+from scrapy import Request
+
+
+class SeleniumRequest(Request):
+    """Scrapy ``Request`` subclass providing additional arguments"""
+
+    def __init__(self, wait_time=None, wait_until=None, screen_shot=False, script=None, r_dict={}, *args, **kwargs):
+        """Initialize a new selenium request
+
+        Parameters
+        ----------
+        wait_time: int
+            The number of seconds to wait.
+        wait_until: method
+            One of the "selenium.webdriver.support.expected_conditions". The response
+            will be returned until the given condition is fulfilled.
+        screen_shot: bool
+            If True, a screenshot of the page will be taken and the data of the screenshot
+            will be returned in the response "meta" attribute.
+        script: str
+            JavaScript code to execute.
+
+        """
+
+        self.wait_time = wait_time
+        self.wait_until = wait_until
+        self.screen_shot = screen_shot
+        self.script = script
+        self.r_dict = r_dict
+
+        super().__init__(*args, **kwargs)
