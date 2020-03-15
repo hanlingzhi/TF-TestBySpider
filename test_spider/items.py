b1009f6394f4f27ca42ab9b424788974e916f9ab
hanlingzhi <greecegods@hotmail.com>
2020-03-15 21:22:30 +0800

    增加比对结果记录表

--- test_spider/items.py
+++ test_spider/items.py
@@ -21,3 +21,21 @@ class ProductItem(scrapy.Item):
         for keys in self.keys():
             str_format += "%s=[%s]|" % (keys, self.__getitem__(keys))
         return str_format
+
+# record item
+class RecordItem(scrapy.Item):
+    tid = scrapy.Field()
+    pid = scrapy.Field()
+    pic_new = scrapy.Field()
+    pic_old = scrapy.Field()
+    pl_ssim = scrapy.Field()
+    cv_ssim = scrapy.Field()
+    pic_pl_diff = scrapy.Field()
+    pic_oc_diff = scrapy.Field()
+    create_time = scrapy.Field()
+
+    def __repr__(self):
+        str_format = ''
+        for keys in self.keys():
+            str_format += "%s=[%s]|" % (keys, self.__getitem__(keys))
+        return str_format
